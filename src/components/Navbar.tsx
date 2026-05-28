c666686a1300398a05ab06bf2632f47df6bde474
kidkurobar <kidkurobar.u@gmail.com>
2026-05-28 22:03:34 +0700

    Add files via upload

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,7 +19,7 @@ export default function Navbar() {
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
           <Link href="/" className="flex items-center gap-2">
-            <img src="/logo.png" alt="Sourcing8" className="h-8 w-auto" />
+            <img src="/sourcing8_final_interlock_1.png" alt="Sourcing8" className="h-8 w-auto" />
             <span className="text-xl font-bold text-gray-900">Sourcing8</span>
           </Link>
 
